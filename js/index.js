a17f268264085639ff9f5f6a1fe986c4fea2faef
taras-fydria <taras.fydria@gmail.com>
2022-10-25 00:15:54 +0300

    taras WTF

--- js/index.js
+++ js/index.js
@@ -1,15 +1,7 @@
-// ;(function () {
-//     document.addEventListener('DOMContentLoaded', async function () {
-//         try {
-//             const response = await fetch('https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap')
-//             const text = response.ok && response.status === 200 ? await response.text() : new Error(response.statusText)
-//             const css = text.replace(/}/g, 'font-display: swap; }')
-//             const head = document.getElementsByTagName('head')[0];
-//             const style = document.createElement('style');
-//             style.appendChild(document.createTextNode(css));
-//             head.appendChild(style);
-//         } catch (e) {
-//             console.log(e)
-//         }
-//     })
-// }())
+;(function () {
+    document.addEventListener('click', function (event) {
+        const btn = event.target.closest('.home-products__favorite')
+        if (btn === null) return
+        btn.classList.toggle('home-products__favorite--active')
+    })
+}())
